547ee42e22cd7281a551599b9636c3dac74f2523
ogurahajimeImac <arzt_1223@ezweb.ne.jp>
2022-03-20 11:09:30 +0900

    atcoder203 A完成

---
+++ atcoder203/A/Main.java
@@ -0,0 +1,22 @@
+// package atcoder203.A;
+import java.util.Scanner;
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int a = sc.nextInt();
+        int b = sc.nextInt();
+        int c = sc.nextInt();
+        if (a == b) {
+            System.out.println(c);
+        }
+        else if (b == c) {
+            System.out.println(a);
+        }
+        else if (c == a) {
+            System.out.println(b);
+        }
+        else {
+            System.out.println(0);
+        }
+    }
+}
